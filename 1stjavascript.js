7f93b4c6a565ffce348780fb0d32649c6c211462
Bikita0102 <139045699+Bikita0102@users.noreply.github.com>
2023-08-13 22:12:23 -0700

    Add files via upload

---
+++ 1stjavascript.js
@@ -0,0 +1,12 @@
+function printhello(){
+    document.getElementById("demo").innerHTML = "i saidd DO NOTT CLICKK!!!!!!"
+    window.alert('Warning-Alert');
+    console.log('hello');
+    let a=5;
+    let b=3;
+    let c=(a+b);
+    document.getElementById("demo1").innerHTML=c;
+    let d=5000;
+    let e=5000*0.15;
+    document.getElementById("demo2").innerHTML=e;
+}
