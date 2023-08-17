4680febb072462a4a1443c030c6f07894aab2b65
Bikita0102 <139045699+Bikita0102@users.noreply.github.com>
2023-08-17 11:02:17 +0545

    Add files via upload

---
+++ javascript3.js
@@ -0,0 +1,17 @@
+//operators
+function operatorexample(){
+    /*  let a=5;
+    let b="20";
+    let result=a+b;
+    document.getElementById("operator").innerHTML=result;
+    //== le value matra check garxa 
+    //=== le value ra type dubai check garxa
+    let a="A";
+    let b="B";
+    document.getElementById("operator").innerHTML=(a>b);*/
+    let age=1;
+    let isold=(age<18)?"young":"old";
+    document.getElementById("operator").innerHTML=isold;
+    let age1="null";
+    let isold1=age1??"young";
+}
