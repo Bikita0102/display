cb4848a8362ac3c6c3d8145fe4a16ed4f541477e
Bikita0102 <139045699+Bikita0102@users.noreply.github.com>
2023-09-04 11:13:38 +0545

    Add files via upload

---
+++ step4.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    /*console.log('Ready Document');
+    $("button").click(function(){
+        console.log('hello');
+        $("#one").hide();//jun id ho teslai matra hide garxa
+    });*/
+    $("#btn1").click(function(){
+        $("#one").hide();
+    });
+    $("#btn2").click(function(){
+        $("#two").hide();
+    });
+    $("#btn3").click(function(){
+        $("#three").hide();
+    });
+});
