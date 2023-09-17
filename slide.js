fda6019008c5b268c3df442cf423ea2d7db7d91f
Bikita0102 <139045699+Bikita0102@users.noreply.github.com>
2023-09-17 11:09:35 +0545

    Add files via upload

---
+++ slide.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    var slides=$(".slide");
+    var dots=$(".dot");
+    var index=0;
+
+    $("#left-arrow").click(function(){
+        index+=(-1);
+        changeslide();
+    });
+
+    $("#right-arrow").click(function(){
+        index+=1;
+        changeslide();
+    });
+
+    changeslide();
+     function changeslide(){
+        if(index>slides.length-1)
+        index=0;
+
+        if(index<0)
+        index=slides.length-1;
+
+        for(let i=0; i<slides.length; i++){
+            slides[i].style.display="none";
+            dots[i].classlist.remove("active");
+        }
+        slides[index].style.display="block";
+        dots[index].classlist.add("active");
+     }
+});
